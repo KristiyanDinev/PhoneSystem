f387d1ec43a8b43d35baae7bd80efea4c158866a
TheGoldenMine <kriskata5000@gmail.com>
2025-03-20 19:29:40 +0200

    Very beginning.

---
+++ src/main/java/me/kristiyandinev/PhoneSystem/PhoneSystemApplication.java
@@ -0,0 +1,13 @@
+package me.kristiyandinev.PhoneSystem;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PhoneSystemApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PhoneSystemApplication.class, args);
+	}
+
+}
---
+++ src/test/java/me/kristiyandinev/PhoneSystem/PhoneSystemApplicationTests.java
@@ -0,0 +1,13 @@
+package me.kristiyandinev.PhoneSystem;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PhoneSystemApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
