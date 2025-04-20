118a87a2f39d4e7e57154e56b4359c6231be1fa0
KristiyanDinev <kriskata50@gmail.com>
2025-04-20 12:44:24 +0300

    Running the project.

--- src/main/java/me/kristiyandinev/PhoneSystem/PhoneSystemApplication.java
+++ src/main/java/me/kristiyandinev/PhoneSystem/PhoneSystemApplication.java
@@ -1,11 +1,8 @@
 package me.kristiyandinev.PhoneSystem;
 
-import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.context.ApplicationContext;
-
 import java.util.logging.Logger;
 
 @SpringBootApplication
@@ -18,11 +15,7 @@ public class PhoneSystemApplication implements CommandLineRunner {
 	}
 
 
-	@Autowired
-	private ApplicationContext applicationContext;
-
 	@Override
 	public void run(String... args) throws Exception {
-		//applicationContext.getBeanDefinitionNames()); beans
 	}
 }
