5db85d2c2ed496a3bf69bbdcaef0b313a43d6c1c
Kristiyan <kriskata50@gmail.com>
2025-05-01 11:32:14 +0300

    Added Javadoc wiki, but without the comments.

--- src/main/java/me/kristiyandinev/PhoneSystem/PhoneSystemApplication.java
+++ src/main/java/me/kristiyandinev/PhoneSystem/PhoneSystemApplication.java
@@ -3,12 +3,11 @@ package me.kristiyandinev.PhoneSystem;
 import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import java.util.logging.Logger;
+
 
 @SpringBootApplication
 public class PhoneSystemApplication implements CommandLineRunner {
 
-	public static Logger logger =  Logger.getGlobal();
 
 	public static void main(String[] args) {
 		SpringApplication.run(PhoneSystemApplication.class, args);
